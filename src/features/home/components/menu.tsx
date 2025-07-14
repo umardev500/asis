b760577b814c06842f03a71359c188d89dbd3f21
Umar <umar.dev500@gmail.com>
2025-07-14 12:02:17 +0700

    change to primary color

--- src/features/home/components/menu.tsx
+++ src/features/home/components/menu.tsx
@@ -9,7 +9,7 @@ export const Menu: React.FC<Props> = () => {
     <>
       <View className="flex-row gap-2 py-6 px-4">
         <View className="items-center flex-1 gap-2">
-          <View className="w-12 h-12 bg-orange-100 items-center justify-center rounded-2xl">
+          <View className="w-12 h-12 bg-primary-50 items-center justify-center rounded-2xl">
             <Icon name="deployed_code_update" size={24} />
           </View>
           <Text className="text-xs text-gray-800 font-medium">
@@ -17,19 +17,19 @@ export const Menu: React.FC<Props> = () => {
           </Text>
         </View>
         <View className="items-center flex-1 gap-2">
-          <View className="w-12 h-12 bg-orange-100 items-center justify-center rounded-2xl">
+          <View className="w-12 h-12 bg-primary-50 items-center justify-center rounded-2xl">
             <Icon name="receipt" size={24} />
           </View>
           <Text className="text-xs text-gray-800 font-medium">Berita</Text>
         </View>
         <View className="items-center flex-1 gap-2">
-          <View className="w-12 h-12 bg-orange-100 items-center justify-center rounded-2xl">
+          <View className="w-12 h-12 bg-primary-50 items-center justify-center rounded-2xl">
             <Icon name="finance_mode" size={24} />
           </View>
           <Text className="text-xs text-gray-800 font-medium">Pajak</Text>
         </View>
         <View className="items-center flex-1 gap-2">
-          <View className="w-12 h-12 bg-orange-100 items-center justify-center rounded-2xl">
+          <View className="w-12 h-12 bg-primary-50 items-center justify-center rounded-2xl">
             <Icon name="attch_money" size={24} />
           </View>
           <Text className="text-xs text-gray-800 font-medium">
@@ -39,7 +39,7 @@ export const Menu: React.FC<Props> = () => {
       </View>
       <View className="flex-row gap-2 px-4 mb-6">
         <View className="items-center flex-1 gap-2">
-          <View className="w-12 h-12 bg-orange-100 items-center justify-center rounded-2xl">
+          <View className="w-12 h-12 bg-primary-50 items-center justify-center rounded-2xl">
             <Icon name="deployed_code_update" size={24} />
           </View>
           <Text className="text-xs text-gray-800 font-medium">
@@ -47,7 +47,7 @@ export const Menu: React.FC<Props> = () => {
           </Text>
         </View>
         <View className="items-center flex-1 gap-2">
-          <View className="w-12 h-12 bg-orange-100 items-center justify-center rounded-2xl">
+          <View className="w-12 h-12 bg-primary-50 items-center justify-center rounded-2xl">
             <Icon name="sunny" size={24} />
           </View>
           <Text className="text-xs text-gray-800 font-medium">
@@ -55,13 +55,13 @@ export const Menu: React.FC<Props> = () => {
           </Text>
         </View>
         <View className="items-center flex-1 gap-2">
-          <View className="w-12 h-12 bg-orange-100 items-center justify-center rounded-2xl">
+          <View className="w-12 h-12 bg-primary-50 items-center justify-center rounded-2xl">
             <Icon name="add" size={24} />
           </View>
           <Text className="text-xs text-gray-800 font-medium">Ambulans</Text>
         </View>
         <View className="items-center flex-1 gap-2">
-          <View className="w-12 h-12 bg-orange-100 items-center justify-center rounded-2xl">
+          <View className="w-12 h-12 bg-primary-50 items-center justify-center rounded-2xl">
             <Icon name="more_horiz" size={24} />
           </View>
           <Text className="text-xs text-gray-800 font-medium">Semua</Text>
