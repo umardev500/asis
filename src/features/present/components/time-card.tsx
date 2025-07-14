147e57a4711476a65ebaeafcce15fed2f16241d1
Umar <umar.dev500@gmail.com>
2025-07-14 10:58:41 +0700

    add real current time in crad

--- src/features/present/components/time-card.tsx
+++ src/features/present/components/time-card.tsx
@@ -1,5 +1,5 @@
 import { Icon } from "@/src/components";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Text, TouchableOpacity, View } from "react-native";
 
 interface Props {
@@ -7,12 +7,40 @@ interface Props {
 }
 
 export const TimeCard: React.FC<Props> = ({ onPress }) => {
+  const [currentTime, setCurrentTime] = useState(new Date());
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000); // update every second
+
+    return () => clearInterval(timer); // cleanup
+  }, []);
+
+  const formatTime = (date: Date) => {
+    return date.toLocaleTimeString("id-ID", {
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: false,
+    });
+  };
+
+  const formatDate = (date: Date) => {
+    return date.toLocaleDateString("id-ID", {
+      weekday: "long",
+      day: "numeric",
+      month: "long",
+      year: "numeric",
+    });
+  };
+
   return (
     <View className="flex-row items-center gap-2 mb-8">
       <View className="bg-orange-50 p-4 h-24 justify-center rounded-xl flex-1">
-        <Text className="text-3xl font-bold text-orange-500">08:51</Text>
+        <Text className="text-3xl font-bold text-orange-500">
+          {formatTime(currentTime)}
+        </Text>
         <Text className="text-xs text-gray-500 mt-1">
-          Selasa, 12 Oktober 2025
+          {formatDate(currentTime)}
         </Text>
       </View>
 
